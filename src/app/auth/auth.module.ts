10561b388396999ebc8d2e754bf5d529b9ccefe9
Jordan Fox <jordan.fox@canstar.com.au>
2022-07-19 11:43:38 +1000

    created shared module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { MaterialModule } from "../material.module";
+import { SharedModule } from "../shared/shared.module";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 
 // each module is standalone - imports from the app.module are not added to any other module and must be imported there also.
 @NgModule({
     declarations: [SignupComponent, LoginComponent],
-    imports: [FormsModule, ReactiveFormsModule, MaterialModule, AngularFireAuthModule],
+    imports: [SharedModule, ReactiveFormsModule, MaterialModule, AngularFireAuthModule],
     exports: []
 })
 export class AuthModule {}
